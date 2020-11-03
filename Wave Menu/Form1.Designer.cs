e01d98f8a92209254f41418fe15309c6eb5e7190
Pringle-Null <73128892+Pringle-Null@users.noreply.github.com>
2020-11-03 10:29:29 -0500

    Upload V1
    V1 uploaded.

---
+++ Wave Menu/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace Wave_Menu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flatClose1 = new FlatUI.FlatClose();
+            this.flatMini1 = new FlatUI.FlatMini();
+            this.flatLabel1 = new FlatUI.FlatLabel();
+            this.flatLabel2 = new FlatUI.FlatLabel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.flatContextMenuStrip1 = new FlatUI.FlatContextMenuStrip();
+            this.formSkin1 = new FlatUI.FormSkin();
+            this.flatTabControl1 = new FlatUI.FlatTabControl();
+            this.Home = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.formSkin1.SuspendLayout();
+            this.flatTabControl1.SuspendLayout();
+            this.Home.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.flatClose1);
+            this.panel1.Controls.Add(this.flatMini1);
+            this.panel1.Controls.Add(this.flatLabel1);
+            this.panel1.Controls.Add(this.flatLabel2);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(914, 49);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // flatClose1
+            // 
+            this.flatClose1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.flatClose1.BackColor = System.Drawing.Color.White;
+            this.flatClose1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.flatClose1.Font = new System.Drawing.Font("Marlett", 10F);
+            this.flatClose1.Location = new System.Drawing.Point(824, 3);
+            this.flatClose1.Name = "flatClose1";
+            this.flatClose1.Size = new System.Drawing.Size(18, 18);
+            this.flatClose1.TabIndex = 6;
+            this.flatClose1.Text = "flatClose1";
+            this.flatClose1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.flatClose1.Click += new System.EventHandler(this.flatClose1_Click);
+            // 
+            // flatMini1
+            // 
+            this.flatMini1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.flatMini1.BackColor = System.Drawing.Color.White;
+            this.flatMini1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+            this.flatMini1.Font = new System.Drawing.Font("Marlett", 12F);
+            this.flatMini1.Location = new System.Drawing.Point(800, 3);
+            this.flatMini1.Name = "flatMini1";
+            this.flatMini1.Size = new System.Drawing.Size(18, 18);
+            this.flatMini1.TabIndex = 5;
+            this.flatMini1.Text = "flatMini1";
+            this.flatMini1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.flatMini1.Click += new System.EventHandler(this.flatMini1_Click);
+            // 
+            // flatLabel1
+            // 
+            this.flatLabel1.AutoSize = true;
+            this.flatLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.flatLabel1.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.flatLabel1.ForeColor = System.Drawing.Color.White;
+            this.flatLabel1.Location = new System.Drawing.Point(83, 9);
+            this.flatLabel1.Name = "flatLabel1";
+            this.flatLabel1.Size = new System.Drawing.Size(86, 37);
+            this.flatLabel1.TabIndex = 2;
+            this.flatLabel1.Text = "Client";
+            this.flatLabel1.Click += new System.EventHandler(this.flatLabel1_Click);
+            // 
+            // flatLabel2
+            // 
+            this.flatLabel2.AutoSize = true;
+            this.flatLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.flatLabel2.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.flatLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.flatLabel2.Location = new System.Drawing.Point(12, 9);
+            this.flatLabel2.Name = "flatLabel2";
+            this.flatLabel2.Size = new System.Drawing.Size(82, 37);
+            this.flatLabel2.TabIndex = 4;
+            this.flatLabel2.Text = "Wave";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel3.Controls.Add(this.panel2);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(5, 438);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.panel2.Location = new System.Drawing.Point(0, 49);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(174, 493);
+            this.panel2.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // flatContextMenuStrip1
+            // 
+            this.flatContextMenuStrip1.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.flatContextMenuStrip1.ForeColor = System.Drawing.Color.White;
+            this.flatContextMenuStrip1.Name = "flatContextMenuStrip1";
+            this.flatContextMenuStrip1.ShowImageMargin = false;
+            this.flatContextMenuStrip1.Size = new System.Drawing.Size(36, 4);
+            // 
+            // formSkin1
+            // 
+            this.formSkin1.BackColor = System.Drawing.Color.White;
+            this.formSkin1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.formSkin1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.formSkin1.Controls.Add(this.flatTabControl1);
+            this.formSkin1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formSkin1.FlatColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.formSkin1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.formSkin1.HeaderColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.formSkin1.HeaderMaximize = false;
+            this.formSkin1.Location = new System.Drawing.Point(0, 0);
+            this.formSkin1.Name = "formSkin1";
+            this.formSkin1.Size = new System.Drawing.Size(845, 436);
+            this.formSkin1.TabIndex = 10;
+            this.formSkin1.Text = "formSkin1";
+            // 
+            // flatTabControl1
+            // 
+            this.flatTabControl1.ActiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.flatTabControl1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.flatTabControl1.Controls.Add(this.Home);
+            this.flatTabControl1.Controls.Add(this.tabPage1);
+            this.flatTabControl1.Controls.Add(this.tabPage2);
+            this.flatTabControl1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.flatTabControl1.ItemSize = new System.Drawing.Size(120, 40);
+            this.flatTabControl1.Location = new System.Drawing.Point(0, 49);
+            this.flatTabControl1.Name = "flatTabControl1";
+            this.flatTabControl1.SelectedIndex = 0;
+            this.flatTabControl1.Size = new System.Drawing.Size(845, 387);
+            this.flatTabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.flatTabControl1.TabIndex = 0;
+            // 
+            // Home
+            // 
+            this.Home.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+            this.Home.Controls.Add(this.panel4);
+            this.Home.Location = new System.Drawing.Point(4, 44);
+            this.Home.Name = "Home";
+            this.Home.Padding = new System.Windows.Forms.Padding(3);
+            this.Home.Size = new System.Drawing.Size(837, 339);
+            this.Home.TabIndex = 0;
+            this.Home.Text = "Home";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+            this.tabPage2.Controls.Add(this.panel6);
+            this.tabPage2.Location = new System.Drawing.Point(4, 44);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(837, 339);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Options";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panel4.Location = new System.Drawing.Point(-44, -103);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(922, 466);
+            this.panel4.TabIndex = 0;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+            this.tabPage1.Controls.Add(this.panel5);
+            this.tabPage1.Location = new System.Drawing.Point(4, 44);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(837, 339);
+            this.tabPage1.TabIndex = 2;
+            this.tabPage1.Text = "Launch";
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panel5.Location = new System.Drawing.Point(-43, -64);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(922, 466);
+            this.panel5.TabIndex = 1;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panel6.Location = new System.Drawing.Point(-43, -64);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(922, 466);
+            this.panel6.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(845, 436);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.formSkin1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Wave Client Menu";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.formSkin1.ResumeLayout(false);
+            this.flatTabControl1.ResumeLayout(false);
+            this.Home.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private FlatUI.FlatLabel flatLabel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private FlatUI.FlatLabel flatLabel2;
+        private FlatUI.FlatClose flatClose1;
+        private FlatUI.FlatMini flatMini1;
+        private System.Windows.Forms.Timer timer1;
+        private FlatUI.FlatContextMenuStrip flatContextMenuStrip1;
+        private FlatUI.FormSkin formSkin1;
+        private FlatUI.FlatTabControl flatTabControl1;
+        private System.Windows.Forms.TabPage Home;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+    }
+}
+
---
+++ Wave Menu/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wave_Menu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void formSkin1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flatButton4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flatButton3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flatLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flatClose1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flatMax1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flatMini1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flatTrackBar2_Scroll(object sender)
+        {
+
+        }
+
+        private void flatTrackBar3_Scroll(object sender)
+        {
+
+        }
+
+        private void flatTrackBar1_Scroll(object sender)
+        {
+
+        }
+
+        private void flatButton3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
